d1a93581fe2bd022524457c715ad30d462971f75
Jreza-r <jr030328@gmail.com>
2024-08-28 17:18:09 +0000

    End of class

--- MadLib.py
+++ MadLib.py
@@ -9,13 +9,13 @@ def main():
   animal1 = input("Enter an animal")
   food1 = input("Enter a food")
   noun1 = input("Enter a noun")
-  noun4 = input("Enter a ")
-  noun5 = input("Enter a ")
-  noun6 = input("Enter a noun")
+  noun2 = input("Enter a noun")
+  animal2 = input("Enter a animal")
+  location1 = input("Enter a noun")
   #Print the story with the user supplied words.
 
-  print("If you give a " + animal1 + "a dozen" + food1 + " he/she will ask for " + noun1 + ".")
-  print("I ")
+  print("If you give a " + animal1 + "a dozen" + food1 + " he/she will ask for a " + noun1 + ".")
+  print("When he/she is done, he/she will " + noun2 + "with their friend the " + animal2 + "until they both go to " + location1 + ".")
 
 #Call the main function if this is the file being run.
 if __name__ == '__main__':
