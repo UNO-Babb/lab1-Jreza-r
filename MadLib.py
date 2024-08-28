82466c3df8775f3ac48d8b09dee0991b31e397bb
Jreza-r <jr030328@gmail.com>
2024-08-28 16:53:59 +0000

    End of class

--- MadLib.py
+++ MadLib.py
@@ -1,15 +1,21 @@
 #MadLib.py
-#Name:
+#Name: Josue Reza-Rivera
 #Date:
-#Assignment:
+#Assignment: Lab 1
 
 def main():
   print("Madlib")
   #Ask user for words
-
+  animal1 = input("Enter an animal")
+  food1 = input("Enter a food")
+  noun1 = input("Enter a noun")
+  noun4 = input("Enter a ")
+  noun5 = input("Enter a ")
+  noun6 = input("Enter a noun")
   #Print the story with the user supplied words.
 
-
+  print("If you give a " + animal1 + "a dozen" + food1 + " he/she will ask for " + noun1 + ".")
+  print("I ")
 
 #Call the main function if this is the file being run.
 if __name__ == '__main__':
